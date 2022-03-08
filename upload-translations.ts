d1d5430e2e6dcdbd835799adea7267f38503ce77
sebastientromp <sebastien.tromp@gmail.com>
2022-03-08 13:56:33 +0100

    add first script for translation files post-process

---
+++ upload-translations.ts
@@ -0,0 +1,32 @@
+import { readdir, readFile, writeFile } from 'fs/promises';
+
+const process = async () => {
+	const files = await readdir('./firestone/');
+	console.log('all files', files);
+	for (const file of files) {
+		console.log('processing', file);
+		const content = await readFile(`./firestone/${file}`, 'utf8');
+		console.log(content);
+		if (!content?.length) {
+			continue;
+		}
+
+		const json = JSON.parse(content);
+		// console.log('json', json, 'test', json.test, 'other', json.other);
+		const jsonWithoutEmpty = removeEmpty(json);
+		// console.log('withoutEmptyStrings', jsonWithoutEmpty);
+		const jsonWithoutEmptyStr = JSON.stringify(jsonWithoutEmpty, null, '\t');
+		await writeFile(`./firestone/${file}`, jsonWithoutEmptyStr);
+		// console.log('written');
+		break;
+	}
+};
+
+process();
+
+const removeEmpty = (obj) => {
+	Object.entries(obj).forEach(
+		([key, val]) => (val && typeof val === 'object' && removeEmpty(val)) || ((val === null || val === '') && delete obj[key])
+	);
+	return obj;
+};
