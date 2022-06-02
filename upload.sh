2b04bfb8cbc0d0adcf5bfe2a58523dd7ba9a44a5
sebastientromp <sebastien.tromp@gmail.com>
2022-06-02 20:21:23 +0200

    add TODO

--- upload.sh
+++ upload.sh
@@ -3,3 +3,4 @@ git commit -am "update source strings" && \
     ts-node upload-translations.ts && \
     aws s3 cp ./firestone/ s3://static.firestoneapp.com/data/i18n/ --recursive --acl public-read && \
     git stash
+# TODO: clear cloudflare cache https://static.firestoneapp.com/data/i18n
