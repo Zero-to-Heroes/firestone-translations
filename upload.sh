d7a4a526e5824f308fa9ea9a2100e1355c7ad2ec
sebastientromp <sebastien.tromp@gmail.com>
2026-02-03 07:51:57 +0100

    update source strings

--- upload.sh
+++ upload.sh
@@ -2,5 +2,6 @@
 git commit -am "update source strings" && \
     npx ts-node upload-translations.ts && \
     aws s3 cp ./firestone/ s3://static.firestoneapp.com/data/i18n/ --recursive --acl public-read && \
+    aws s3 cp ./firestone/ s3://static.zerotoheroes.com/hearthstone/data/i18n/ --recursive --acl public-read && \
     git stash
 # TODO: clear cloudflare cache https://static.firestoneapp.com/data/i18n
